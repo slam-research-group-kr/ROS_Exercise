9ee960e9129339d4e9789396a4609fd2ba599cef
Dong-Won Shin <dongwonshin@gist.ac.kr>
2019-03-15 14:13:08 +0900

    init

--- src/arm_mover.cpp
+++ src/arm_mover.cpp
@@ -76,13 +76,12 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "arm_mover");
     ros::NodeHandle n;
 
-    // Define two publishers to publish std_msgs::Float64 messages on joints respective topics
-    joint1_pub = n.advertise<std_msgs::Float64>("/simple_arm/joint_1_position_controller/command", 10);
-    joint2_pub = n.advertise<std_msgs::Float64>("/simple_arm/joint_2_position_controller/command", 10);
+    // Todo: Define two publishers to publish std_msgs::Float64 messages on joints respective topics
+    
 
-    // Define a safe_move service with a handle_safe_move_request callback function
-    ros::ServiceServer service = n.advertiseService("/arm_mover/safe_move", handle_safe_move_request);
-    ROS_INFO("Ready to send joint commands");
+
+    // Todo: Define a safe_move service with a handle_safe_move_request callback function
+    
 
     // Handle ROS communication events
     ros::spin();
